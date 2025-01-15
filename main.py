955cd84b4d3bd737c8aed5b0061912c3d918f9cd
bea-ek <ekaterinaboldashova@yandex.com>
2025-01-15 20:52:45 +0500

    я сделала задний фон как тайлы по рядам и картинки сделала рядами, ещё набросок камеры

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import pygame
 
 FPS = 50
 
+cell_width = cell_height = 120
+
+level = ['.', '.', '#', '#', '#', '#', '#', '#']
+for i in range(9):
+    level.insert(0, choice(('.', '.', '.', '=', '=', '#', '~')))
+
 
 def terminate():
     pygame.quit()
@@ -41,82 +47,142 @@ def load_image(name):
     return image
 
 
-class Board:
-    # создание поля
-    def __init__(self):
-        self.width = 5  # Кол-во клеток в ширину
-        self.height = 7  # в высоту
-        self.board = [Grass() for _ in range(self.height)]
-        # Значения положения поля по умолчанию
-        self.left = 0
-        self.top = 0
-        for i in range(4):
-            cell = choice((Grass(), Grass(), Grass(), River(), Road(), Road(), Railway()))
-            self.board[i] = cell
-
-    # настройка положения поля
-    def set_view(self, left, top):
-        self.left = left
-        self.top = top
-
-    # отрисовка
-    def render(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                screen.blit(self.board[y].image, (
-                    x * 120 + self.left, y * 120 + self.top))
-            if self.board[y].__class__ == Grass:
-                for x in range(self.width):
-                    screen.blit(self.board[y].row[x], (
-                        x * 120 + self.left, y * 120 + self.top))
+def new_row():
+    for i in range(17):
+        level[i - 1] = level[i]
+    level[16] = choice(('.', '.', '.', '=', '=', '#', '~'))
 
 
-class Grass(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = load_image("grass.jpg")
-        self.image = pygame.transform.scale(self.image, (120, 120))
-        self.row = [choice((Bush().image, Stone().image, self.image, self.image)),
-                    choice((Bush().image, Stone().image, self.image, self.image)), self.image,
-                    choice((Bush().image, Stone().image, self.image, self.image)),
-                    choice((Bush().image, Stone().image, self.image, self.image))]
+def generate_level():
+    for y, row in enumerate(level[5:-5]):
+        if row == '.':
+            Tile('grass', y)
+        elif row == '_':
+            Tile('border', y)
+        elif row == '=':
+            Tile('road', y)
+        elif row == '#':
+            Tile('railway', y)
+        elif row == '~':
+            Tile('river', y)
+    # return Player(2, 10)
+
+
+# class Board:
+#     # создание поля
+#     def __init__(self):
+#         self.width = 5  # Кол-во клеток в ширину
+#         self.height = 7  # в высоту
+#         self.board = [Grass() for _ in range(self.height)]
+#         # Значения положения поля по умолчанию
+#         self.left = 0
+#         self.top = 0
+#         for i in range(4):
+#             cell = choice((Grass(), Grass(), Grass(), River(), Road(), Road(), Railway()))
+#             self.board[i] = cell
+#
+#     # настройка положения поля
+#     def set_view(self, left, top):
+#         self.left = left
+#         self.top = top
+#
+#     # отрисовка
+#     def render(self, screen):
+#         for y in range(self.height):
+#             for x in range(self.width):
+#                 screen.blit(self.board[y].image, (
+#                     x * 120 + self.left, y * 120 + self.top))
+#             if self.board[y].__class__ == Grass:
+#                 for x in range(self.width):
+#                     screen.blit(self.board[y].row[x], (
+#                         x * 120 + self.left, y * 120 + self.top))
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = load_image(f"{tile_type}.jpg")
+        self.image = pygame.transform.scale(self.image, (120*5, 120))
+        self.rect = self.image.get_rect().move(
+        0, cell_height * pos_y)
+
+
+# class Grass(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#         self.image = load_image("grass.jpg")
+#         self.image = pygame.transform.scale(self.image, (120, 120))
+#         self.row = [choice((Bush().image, Stone().image, self.image, self.image)),
+#                     choice((Bush().image, Stone().image, self.image, self.image)), self.image,
+#                     choice((Bush().image, Stone().image, self.image, self.image)),
+#                     choice((Bush().image, Stone().image, self.image, self.image))]
 
 
 class Bush(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
         self.image = load_image("bush.png")
         self.image.set_colorkey((0, 0, 0))
         self.image = pygame.transform.scale(self.image, (120, 120))
+        self.rect = self.image.get_rect(left=x, top=y)
 
 
 class Stone(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
         self.image = load_image("stone.jpg")
         self.image.set_colorkey((0, 0, 0))
         self.image = pygame.transform.scale(self.image, (120, 120))
+        self.rect = self.image.get_rect(left=x, top=y)
 
 
-class River(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = load_image("river2.jpg")  # либо river.jpg
-        self.image = pygame.transform.scale(self.image, (120, 120))
+# class River(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#         self.image = load_image("river2.jpg")  # либо river.jpg
+#         self.image = pygame.transform.scale(self.image, (120, 120))
+#
+#
+# class Road(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#         self.image = load_image("road.jpg")
+#         self.image = pygame.transform.scale(self.image, (120, 120))
+#
+#
+# class Railway(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#         self.image = load_image("railway.jpg")
+#         self.image = pygame.transform.scale(self.image, (120, 120))
 
 
-class Road(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = load_image("road.jpg")
-        self.image = pygame.transform.scale(self.image, (120, 120))
+# class Player(pygame.sprite.Sprite):
+#     def __init__(self, x, y):
+#         super().__init__(player_group, all_sprites)
+#         self.rect = self.image.get_rect(left=x, top=y)
 
 
-class Railway(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = load_image("railway.jpg")
-        self.image = pygame.transform.scale(self.image, (120, 120))
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self, width, height):
+        self.dx = 0
+        self.dy = 0
+        self.width = width
+        self.height = height
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dy = -(target.rect.y + target.rect.h // 2 - self.height // 2)
+
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
 
 def main():
@@ -124,15 +190,14 @@ def main():
     size = 600, 840
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Инициализация игры')
-    board = Board()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 0, 0))
-
-        board.render(screen)
+        generate_level()
+        all_sprites.draw(screen)
         pygame.display.flip()
     pygame.quit()
 
